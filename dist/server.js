28436a05278e09d8e63d39e7ef9a88156d1dc8e5
Jean Zahn <jeanozahn@gmail.com>
2024-05-15 13:19:15 -0400

    Last-Step-Slide-6

---
+++ dist/server.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const express = require('express');
+const app = express();
+app.use((req, res, next) => {
+  console.log('will run before any route');
+  next(new Error('failed!'));
+});
+app.use((err, req, res, next) => {
+  console.log('something goes wrong');
+  res.status(500).send(err.message);
+});
+app.get('/', function (req, res, next) {
+  console.log('route / called');
+  res.send('Hello World!');
+});
+app.listen(3000, () => {
+  console.log('app is running');
+});
